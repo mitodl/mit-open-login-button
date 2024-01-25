6b52cdc04c6cbd8f4679febf422593fd93c6d1a1
Carey Gumaer <gumaerc@mit.edu>
2024-01-25 18:41:47 -0500

    fmt

--- index.js
+++ index.js
@@ -8,43 +8,51 @@
  * @param {String} buttonClass The CSS class(es) to assign to the button
  * @param {String} loggedInTextClass The CSS class(es) to assign to the logged-in status text
  */
-export function initLoginButton(containerId, baseUrl, buttonText = "Login", buttonClass = "", loggedInTextClass = "") {
+export function initLoginButton(
+  containerId,
+  baseUrl,
+  buttonText = "Login",
+  buttonClass = "",
+  loggedInTextClass = ""
+) {
   const container = document.getElementById(containerId)
   const parsedBaseUrl = new URL(baseUrl)
-  const reconstructedBaseUrl = `${parsedBaseUrl.protocol}//${parsedBaseUrl.hostname}${parsedBaseUrl.port !== '' ? `:${parsedBaseUrl.port}` : ''}`
+  const reconstructedBaseUrl = `${parsedBaseUrl.protocol}//${parsedBaseUrl.hostname}${parsedBaseUrl.port !== "" ? `:${parsedBaseUrl.port}` : ""}`
   const apiUrl = `${reconstructedBaseUrl}/api/v0/users/me?format=json`
   const loginUrl = `${reconstructedBaseUrl}/login/ol-oidc/`
   fetch(apiUrl, {
-    method: "GET",
+    method:      "GET",
     credentials: "include",
-    mode: "cors",
-    headers: {
-      "Accept": "application/json",
-    }
-  }).then(response => {
-    if (!response.ok) {
-      // create the login button
-      const linkButton = document.createElement('a')
-      const linkText = document.createTextNode(buttonText)
-      linkButton.appendChild(linkText)
-      linkButton.title = buttonText
-      linkButton.href = loginUrl
-      if (buttonClass !== "") {
-        linkButton.classList.add(...buttonClass.split(' '))
-      }
-      container.appendChild(linkButton)
-    }
-    return response.json()
-  }).then(data => {
-    if (data['username'] !== undefined) {
-      // display the logged in user
-      const userName = data['username']
-      const loggedInText = document.createElement('span')
-      if (loggedInTextClass !== "") {
-        loggedInText.classList.add(...loggedInTextClass.split(' '))
-      }
-      loggedInText.textContent = `Logged in as: ${userName}`
-      container.appendChild(loggedInText)
+    mode:        "cors",
+    headers:     {
+      Accept: "application/json"
     }
   })
+    .then(response => {
+      if (!response.ok) {
+        // create the login button
+        const linkButton = document.createElement("a")
+        const linkText = document.createTextNode(buttonText)
+        linkButton.appendChild(linkText)
+        linkButton.title = buttonText
+        linkButton.href = loginUrl
+        if (buttonClass !== "") {
+          linkButton.classList.add(...buttonClass.split(" "))
+        }
+        container.appendChild(linkButton)
+      }
+      return response.json()
+    })
+    .then(data => {
+      if (data["username"] !== undefined) {
+        // display the logged in user
+        const userName = data["username"]
+        const loggedInText = document.createElement("span")
+        if (loggedInTextClass !== "") {
+          loggedInText.classList.add(...loggedInTextClass.split(" "))
+        }
+        loggedInText.textContent = `Logged in as: ${userName}`
+        container.appendChild(loggedInText)
+      }
+    })
 }
